c35fdc14f51e2f985d60ef74258a8b77bfe85a60
robotsintheattic <katherine.r.johnson@gmail.com>
2017-06-04 17:28:19 -0600

    copy in all files from pt 1 and run seeds & migrations

---
+++ server/routes/classifieds.js
@@ -0,0 +1,83 @@
+
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const knex = require('../knex')
+
+router.get('/classifieds', (req, res, next) => {
+  knex('classifieds')
+  .select('id', 'title', 'title', 'description', 'price', 'item_image')
+  .orderBy('id')
+  .then((data) => {
+    res.send(data);
+  })
+  .catch((err) => {
+    next(err);
+  });
+});
+
+router.get('/classifieds/:id', (req, res, next) => {
+  let id = Number.parseInt(req.params.id)
+
+  knex('classifieds')
+    .select(['id', 'title', 'description', 'price', 'item_image'])
+    .where('id', id)
+    .then((data) => {
+      res.send(data[0]);
+    })
+    .catch((err) => {
+      next(err);
+    })
+})
+
+router.post('/classifieds', (req, res, next) => {
+  knex('classifieds')
+  .returning(['id', 'title', 'description', 'price', 'item_image'])
+  .insert({
+    title: req.body.title,
+    description: req.body.description,
+    price: req.body.price,
+    item_image: req.body.item_image
+  })
+  .then((data) => {
+    res.send(data[0]);
+  })
+  .catch((err) => {
+    next(err);
+  });
+});
+
+router.patch('/classifieds/:id', (req, res, next) => {
+  let id = Number.parseInt(req.params.id)
+
+  knex('classifieds')
+    .where('id', id)
+    .returning(['id', 'title', 'description', 'price', 'item_image'])
+    .update(req.body)
+    .then((data) => {
+      res.send(data[0])
+      // "Ad Updated!"
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.delete('/classifieds/:id', (req, res, next) => {
+  let id = Number.parseInt(req.params.id)
+
+  knex('classifieds')
+    .where('id', req.params.id)
+    .returning(['id', 'title', 'description', 'price', 'item_image'])
+    .del()
+    .then((data) => {
+      res.send(data[0]);
+    // "Ad Deleted!"
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+module.exports = router;
